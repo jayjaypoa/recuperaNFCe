e8095d5d2b5f222dcd3c7d5aedcb8a6cd83587db
Joacir Junior <contato@joacirjunior.com>
2017-04-12 02:54:42 -0300

    Criada classe SefazRS para realizar parse do RS. Corrigido valor total e valor unitário do produto.

--- src/parse/SefazRS.java
+++ src/parse/SefazRS.java
@@ -1,7 +1,294 @@
 package parse;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import cupom.NFCe;
+import cupom.Produto;
+
 public class SefazRS {
 	
+	private File file;
+	private String html;	
+	private Document doc;
 	
+	public SefazRS(String html){
+		this.html = html;
+	}
+	
+	public SefazRS(File file){
+		this.file = file;
+	}
+	
+	public void execute() throws IOException {
+		
+		// parse HTML
+		if(this.html == null && this.file.exists()){
+			this.doc = Jsoup.parse(this.file, "ISO-8859-1"); // por arquivo
+		} else {
+			this.doc = Jsoup.parse(this.html); // por string 
+		}
+		
+		// cria o objeto da nota fiscal
+		NFCe notaFiscal = new NFCe();        
+		
+		//Element content = doc.getElementById("content");
+        //Elements links = content.getElementsByTag("td");
+        Elements divs = doc.getElementsByTag("div");
+        for (Element div : divs) {
+/*
+        	// Div NFe - Inicio
+        	if(div.attr("id").toUpperCase().contentEquals("NFE")){
+        		System.out.println(" #################################################### ");
+        		System.out.println(" NFE : ");
+        		System.out.println(div);
+        	} // Div NFe - Fim        	
+        	
+        	// Div EMITENTE - Inicio
+        	if(div.attr("id").toUpperCase().contentEquals("EMITENTE")){
+        		System.out.println(" #################################################### ");
+        		System.out.println(" EMITENTE : ");
+        		System.out.println(div);
+        	} // Div EMITENTE - Fim
+        	
+        	// Div DESTINATARIO / CLIENTE - Inicio
+        	if(div.attr("id").toUpperCase().contentEquals("DESTREM")){
+        		System.out.println(" #################################################### ");
+        		System.out.println(" DESTREM : ");
+        		System.out.println(div);
+        	} // Div DESTINATARIO / CLIENTE - Fim
+        	
+        	// Div TOTAIS - Inicio
+        	if(div.attr("id").toUpperCase().contentEquals("TOTAIS")){
+        		System.out.println(" #################################################### ");
+        		System.out.println(" TOTAIS : ");
+        		System.out.println(div);
+        	} // Div TOTAIS - Fim
+        	
+        	// Div TRANSPORTE - Inicio
+        	if(div.attr("id").toUpperCase().contentEquals("TRANSPORTE")){
+        		System.out.println(" #################################################### ");
+        		System.out.println(" TRANSPORTE : ");
+        		System.out.println(div);
+        	} // Div TRANSPORTE - Fim
+        	
+        	// Div COBRANCA - Inicio
+        	if(div.attr("id").toUpperCase().contentEquals("COBRANCA")){
+        		System.out.println(" #################################################### ");
+        		System.out.println(" COBRANCA : ");
+        		System.out.println(div);
+        	} // Div COBRANCA - Fim
+        	
+        	// Div INFORMACOES ADICIONAIS - Inicio
+        	if(div.attr("id").toUpperCase().contentEquals("INF")){
+        		System.out.println(" #################################################### ");
+        		System.out.println(" INF : ");
+        		System.out.println(div);
+        	} // Div INFORMACOES ADICIONAIS - Fim
+        	
+        	// System.out.println(div);
+*/        	
+        
+        	// Parse dos PRODUTOS
+        	if(div.attr("id").toUpperCase().contentEquals("PROD")){
+        		ArrayList<Produto> listagem = this.processarProdutos(div);
+            	notaFiscal.setProdutos(listagem);
+        	}
+        	
+        } // fim do for das divs
+        
+        // imprime no console a lista de produtos
+        System.out.println("QTD DE PRODUTOS = " + notaFiscal.getProdutos().size());
+        int numPrd = 0;
+        for(Produto p : notaFiscal.getProdutos()){
+        	numPrd++;
+        	System.out.println("PRODUTO " + numPrd + " ===> " + p.toString());
+        }
+		
+	} // fim do execute
+	
+	
+	private ArrayList<Produto> processarProdutos(Element div){
+		
+		// cria a listagem de produtos
+		ArrayList<Produto> listaProdutos = new ArrayList<Produto>();
+		
+		System.out.println(" #################################################### ");
+		System.out.println(" PROD : ");
+		
+		int qtdProdutos = 0;
+		int nroTabela = 0;
+		
+		Integer id = 0;
+		String codigo = "";
+		String NCM = "";
+		String EAN = "";
+		String descricao = "";
+		Double quantidade = 0.0;
+		String unidade = "";
+		Double valorUnitario = 0.0;
+		Double valorTotal = 0.0;
+		
+		Elements tables = div.getElementsByTag("table");
+		for (Element table : tables){
+			
+			// ignora a tabela de cabecalho (primeira tabela - indice 0)
+			if (nroTabela > 0){
+				
+				// Imprime as tables no console...
+    			// System.out.println("TABELA " + nroTabela + ":\n" + table.html() + "\n\n");
+    			
+				// contador, descricao, unidade e valor
+				// Class : fixo-prod-serv-numero,
+				// fixo-prod-serv-descricao, fixo-prod-serv-qtd, 
+				// fixo-prod-serv-uc, fixo-prod-serv-vb
+				if (nroTabela == 1){
+					
+					// System.out.println("ENTROU NA TABELA 1");
+					// System.out.println("NRO TABELA : " + nroTabela + "\n" + table.html() +"\n\n");
+					
+					Elements colunas = table.getElementsByTag("td");
+					for(Element coluna : colunas){
+						
+						String atributoColuna = coluna.attr("class").toUpperCase();
+						
+						// contador
+						if (atributoColuna.contentEquals("FIXO-PROD-SERV-NUMERO")){
+							// System.out.println("A = " + coluna.text());
+							id = Integer.parseInt(coluna.text());
+			        		codigo = id.toString();					        		
+						// descricao									
+						} else if (atributoColuna.contentEquals("FIXO-PROD-SERV-DESCRICAO")){
+							// System.out.println("B = " + coluna.text());
+							descricao = coluna.text();
+						// quantidade
+						} else if (atributoColuna.contentEquals("FIXO-PROD-SERV-QTD")){
+							// System.out.println("C  = " + coluna.text());
+			        		quantidade = Double.parseDouble(coluna.text().replace(',', '.'));
+						// unidade
+						} else if (atributoColuna.contentEquals("FIXO-PROD-SERV-UC")){
+							// System.out.println("D = " + coluna.text());
+							unidade = coluna.text();
+						// valor
+						} else if (atributoColuna.contentEquals("FIXO-PROD-SERV-VB")){
+							// System.out.println("E = " + coluna.text());
+							valorTotal = Double.parseDouble(coluna.text().replace(',', '.'));
+							valorUnitario = valorTotal/quantidade;
+						}
+						
+					} // fim do for-each das colunas
+				
+				// Codigo do produto, NCM, CEST,
+				// CFOP, Valor Desconto, Valor Total Frete,
+				// Valor Seguro, EAN, Unidade Comercial,
+				// Quantidade Comercial, EAN Tributavel,
+				// Valor Unitario de Comercializacao,
+				// Nro Pedido Compra, PIS, CST e COFINS 
+				} else if (nroTabela == 2){
+					
+					// System.out.println("ENTROU NA TABELA 2");
+					// System.out.println("NRO TABELA : " + nroTabela + "\n" + table.html() +"\n\n");
+					
+					int contAux = 0;
+					Elements colunas = table.getElementsByTag("td");
+					for(Element colunaAux : colunas){
+						if (contAux > 0){
+							
+							// obtem o nome da coluna
+							String nomeColuna = colunaAux.getElementsByTag("label").text();
+							nomeColuna = nomeColuna.toUpperCase().trim();
+							String valorColuna = colunaAux.getElementsByTag("span").text();
+							
+							// System.out.println("$$$" + nomeColuna + " - " + valorColuna + "$$$");
+							
+							if (nomeColuna.equalsIgnoreCase("CÓDIGO DO PRODUTO")){
+								codigo = valorColuna;
+							} else if (nomeColuna.equalsIgnoreCase("CÓDIGO NCM")){
+								NCM = valorColuna;
+							} else if (nomeColuna.equalsIgnoreCase("CÓDIGO EAN COMERCIAL")){
+								EAN = valorColuna;
+							}
+							
+						}
+						contAux++;
+					}
+					
+				// Codigo do Produto, NCM, CEST, CFOP
+				} else if (nroTabela == 3){
+					
+					// IGNORAR - Motivo: DADOS NAO NECESSARIOS
+					// System.out.println("ENTROU NA TABELA 3");
+					
+				// EAN Comercial, Unidade Comercial, Quantidade Comercial,
+				// EAN Tributavel, Unidade Tributavel, Quantidade Tributavel
+				// e Valor Unitario de Comercializacao
+				} else if (nroTabela == 4){
+					
+					// IGNORAR - Motivo: DADOS NAO NECESSARIOS
+					// System.out.println("ENTROU NA TABELA 4");
+					
+				// Origem da Mercadoria, Tributacao ICMS,
+				// Valor da BC do ICMS ST retido e 
+				// Valor ICMS ST retido
+				} else if (nroTabela == 5){
+					
+					// IGNORAR - Motivo: DADOS NAO NECESSARIOS
+					// System.out.println("ENTROU NA TABELA 5");
+					
+				// CST
+				} else if (nroTabela == 6){
+					
+					// IGNORAR - Motivo: DADOS NAO NECESSARIOS
+					// System.out.println("ENTROU NA TABELA 6");
+						        							
+				// CST
+				} else if (nroTabela == 7){
+
+					// IGNORAR - Motivo: DADOS NAO NECESSARIOS	    
+					// System.out.println("ENTROU NA TABELA 7");
+				
+				} // fim dos if-else das tables dos produtos
+					        			
+			} // fim do if nroTabela > 0
+			
+			// incrementa variavel de controle
+			nroTabela++;
+			        			
+			// Cada produto possui no maximo 7 tables,
+			// portanto, recomeca a contagem caso atinga este limite
+			if (nroTabela > 7){
+				
+				// cria o objeto do produto
+				Produto produto = new Produto();
+				produto.setId(id);
+				produto.setCodigo(codigo);
+				produto.setDescricao(descricao);
+				produto.setEAN(EAN);
+				produto.setNCM(NCM);
+				produto.setQuantidade(quantidade);
+				produto.setUnidade(unidade);
+				produto.setValorUnitario(valorUnitario);
+				produto.setValorTotal(valorTotal);
+				
+				// adiciona o produto gerado na lista de produtos
+				listaProdutos.add(produto);
+				
+				// redefine a variavel de controle
+				// para navegar nos demais produtos
+				nroTabela = 1;
+				
+			} // fim do if (nroTabela > 7)
+			
+		} // fim for-each dos tables
+        
+        return listaProdutos;
+		
+	} // fim do processarProdutos()
 
 }
--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,21 +1,19 @@
 package principal;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStream;
+import java.io.File;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import cupom.NFCe;
+import cupom.Produto;
+import parse.SefazRS;
 import util.Localidade;
 
 public class Main {
@@ -33,103 +31,42 @@ public class Main {
 		
 		String urlFinal = "";
 		String chave = "43170293015006002833651120000969211356021104";
+		// String chave = "43170433014556019024652090000079951878545252";
+		// String chave = "43170402765561000379650100000283951000283958";
 		
 		Localidade local = new Localidade("RS");
-		System.out.println(local.getURL());
+		//System.out.println(local.getURL());
 		
 		urlFinal = local.getURL().replace("@CHAVE@", chave);
 		
-		System.out.println(urlFinal);
+		System.out.println("Processando NFCe : " + urlFinal);
 		
 		System.out.println("1 =========================================================");
-		
+
 		URL paginaBase = new URL(urlFinal);
         BufferedReader in = new BufferedReader(
         		new InputStreamReader(paginaBase.openStream()));
 
         String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
+        String html = "";
+        while ((inputLine = in.readLine()) != null){
+        	html += inputLine;
+            // System.out.println(inputLine);            
+        }
         in.close();
-        
+		SefazRS sefazRS = new SefazRS(html);
+		
         System.out.println("2 =========================================================");
         
-        String url = urlFinal;
-        URL obj = new URL(url);
-
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        
-        // add request header
-        con.setRequestMethod("POST");
-        con.setRequestProperty("User-Agent", "Mozilla/5.0  etc.");
-        con.setRequestProperty("Accept-Language", "en-US,en;q=0.8");
-        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        con.setRequestProperty("Connection", "keep-alive");
-        con.setRequestProperty("Accept", "*/*");
-        con.setRequestProperty("Accept-Encoding", "gzip,deflate,sdch");
-
-     // Send post request
-        con.setDoOutput(true);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes("chaveNFe=43170293015006002833651120000969211356021104");
-        wr.flush();
-        wr.close();
-
-        int responseCode = con.getResponseCode();
-
-        BufferedReader bufferIn = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        inputLine = "";
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = bufferIn.readLine()) != null) {
-            response.append(inputLine);
-        }
-        bufferIn.close();
-        
-        //if password is not correct, my form should return false
-//        if (response.toString().equals("false"))
-//            System.out.println("FALSE  <----------");
-//        else 
-//        	System.out.println("TRUE <----------");
-
+        // Abre o HTML a partir do arquivo para não bloquear o IP de origem na SEFAZ
+        // File file = new File("C:\\temp\\NFCe_EXEMPLO.txt");
+        //SefazRS sefazRS = new SefazRS(file);
         
         System.out.println("3 =========================================================");
         
+        // realzia o processamento da NF
+        sefazRS.execute();
         
-        //Making http get request
-        HttpClient httpClientDefault1 = new DefaultHttpClient();
-        HttpPost httpPost = new HttpPost(urlFinal);
-        
-        //setup headers (Server understand request throw by some browser)
-        //httpPost.setHeader("Connection", "keep-alive");
-        //httpPost.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; rv:9.0.1) Gecko/20100101 Firefox/9.0.1");
-        //httpPost.setHeader("Accept", " text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-        //httpPost.setHeader("Accept-Language", "en-us,en;q=0.5");
-        //httpPost.setHeader("Host", "ec2-23-20-44-83.compute-1.amazonaws.com");
-        //httpPost.setHeader("Referer",resultUrl+resultUrlAsp);
-        
-        // Set parameters
-        ArrayList<NameValuePair> nameValuePair = new ArrayList<NameValuePair>();
-        nameValuePair.add(new BasicNameValuePair("key",""));
-        nameValuePair.add(new BasicNameValuePair("txtenroll","095020693015"));
-        httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
-        //Send request
-
-        HttpResponse httpRespnse = httpClientDefault1.execute(httpPost);
-        //Get Response body
-
-        if(httpRespnse.getStatusLine().getStatusCode() != 200) {
-            InputStream in2 =  httpRespnse.getEntity().getContent();
-            byte b[] = new byte[1024] ;
-            StringBuilder html = new StringBuilder("");
-            while(in2.read(b) != -1) {
-                html.append((new String(b)).toString());
-                b = new byte[1024];
-            }
-            System.out.println(html);
-        }
-	
 	} // fim do executarRS()
 
 }
